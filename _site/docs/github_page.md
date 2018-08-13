7867a3bcb6ebdf9e8c75517f1dec284dd744fa14
eleksbai_mi_air <eleksbai@gmail.com>
2018-08-13 20:45:45 +0800

    测试主题，删除临时文件

--- _site/docs/github_page.md
+++
@@ -1,3 +0,0 @@
-# github page 使用相关
-
-[jekyll官网文档参考](https://jekyllrb.com/docs/home/)
--- _site/local_run.py
+++
@@ -1,8 +0,0 @@
-import subprocess, os
-
-
-# 启动本地服务
-# chcp 65001
-# bundle exec jekyll server
-print(os.getcwd(), 'http://127.0.0.1:4000/')
-subprocess.run(['bundle', 'exec', 'jekyll', 'server'])
